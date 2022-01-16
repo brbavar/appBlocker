a16a72d000b433380b34c2033143c642e2f60cc8
brbavar <ben.bavar@gmail.com>
2022-01-16 00:39:04 -0500

    finished debugging and added commands to kill any active terminal sessions

--- block.cpp
+++ block.cpp
@@ -1,22 +1,30 @@
 #include <string>
 
 int main(int argc, char* argv[]) {
+    system("taskkill /f /im blockStarter.exe");
+    system("taskkill /f /im WindowsTerminal.exe");
+    system("taskkill /f /im blockStarter.exe");
     while(1) {
-        std::string cmd = "taskkill /F /im ";
+        std::string cmd = "taskkill /f /im ";
         /* Skip first command-line arg, which is not the name of
            an app we were asked to block, but rather the string
            "appBlocker.exe" that runs this program */
         int i = 1;
-        while(i < argc - 1) {
-            char c = *argv[i];
+        while(i < argc) {
+            int j = 0;
+            char c = argv[i][j];
             while(c != ' ') {
-                if((int)c > 32 && (int)c != 127 && (int)c != 255) {
+                if((int)c > 32 && (int)c < 127) {
                     if(c == ':')
                         c = ' ';
                     cmd += c;
                 }
-                i++;
+                else
+                    break;
+                j++;
+                c = argv[i][j];
             }
+            i++;
             system(cmd.c_str());
             cmd = cmd.substr(0, 16);
         }
--- main.cpp
+++ main.cpp
@@ -15,10 +15,13 @@ std::vector<std::string> getApps() {
         std::getline(std::cin, apps[i]);
         for(int j = 0; j < apps[i].size(); j++) {
             char c = apps[i][j];
-            if((int)c > 32 && (int)c != 127 && (int)c != 255)
+            if((int)c > 32 && c != 127 && (int)c != 255)
                 inputEmpty = false;
-            else
+            else {
+                if(c == '\r' || c == '\n')
+                    break;
                 apps[i][j] = ':';
+            }
         }
         i++;
     }
@@ -30,10 +33,11 @@ int main() {
     std::cout << "\nLet's start blocking some apps. Type the name \nof the app you want to block, and then \npress "
         << "the enter/return key exactly once. \nRepeat this process for every app you want \nto block. Once you "
         << "have entered the final \napp's name, press enter/return twice to start \nblocking all the apps you've "
-        << "listed." << std::endl;
+        << "listed.\n" << std::endl;
     auto apps = getApps();
     std::string cmd = "g++ block.cpp -o appBlocker.exe && appBlocker.exe";
     for(std::string s : apps)
         cmd += " " + s;
     system(cmd.c_str());
+    std::cout << "end main" << std::endl;
 }
