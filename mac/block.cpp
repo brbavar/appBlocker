2d14e5c7d4bb826b452260b7b81970672bc733df
brbavar <ben.bavar@gmail.com>
2022-01-20 04:35:46 -0500

    complete overhaul

--- mac/block.cpp
+++ mac/block.cpp
@@ -1,94 +1,83 @@
-#include <fstream>
 #include <string>
-#include <algorithm>   // std::find 
 #include <vector>
 
-std::vector<std::string> getArgs(int, char* []);
-std::vector<std::string> saveApps(std::vector<std::string>, std::vector<std::string>);
-std::vector<std::string> getSavedApps();
 
-std::vector<std::string> getArgs(int argc, char* argv[]) {
-    std::vector<std::string> args;
-    args.reserve(argc - 1);
-    std::string arg = "";
-    /* Skip first command-line arg, which is not the name of
-           an app we were asked to block, but rather the string
-           "appBlocker.exe" that runs this program */
-    int i = 1;
-    while (i < argc) {
-        int j = 0;
-        char c = argv[i][j];
-        while (c != ' ') {
-            if ((int)c > 32 && (int)c < 127) {
-                if (c == ':')
-                    c = ' ';
-                arg += c;
-            }
-            else
-                break;
-            j++;
-            c = argv[i][j];
-        }
-        if(arg != "")
-            args.push_back(arg);
-        arg = "";
-        i++;
-    }
-    return args;
+std::string run(std::string, int);
+std::string getPlistPath(std::string, std::string);
+std::vector<std::string> block(std::vector<std::string>);
+void kill(std::vector<std::string>);
+
+std::string run(std::string cmd, int size = 100) {
+    std::string output = "";
+    char buf[size];
+    FILE* stream = popen(cmd.c_str(), "r");
+    if (stream)
+        while (!feof(stream))
+            if (fgets(buf, size, stream) != NULL)
+                output += buf;
+    pclose(stream);
+    return output;
 }
 
-std::vector<std::string> saveApps(std::vector<std::string> newApps, std::vector<std::string> savedApps) {
-    std::vector<std::string> apps;
-    apps.reserve(newApps.size() + savedApps.size());
-    std::ofstream log("blocklist.txt", std::ios_base::app);
-    if (log.is_open())
-        for (std::string s : newApps) {
-            if (find(savedApps.begin(), savedApps.end(), s) == savedApps.end()) {
-                log << s << '\n';
-                apps.push_back(s);
+std::string getPlistPath(std::string app, std::string dir) {
+    std::string xtnsnMtchs = run("ls " + dir + " | grep '" + app + ".app'");
+    if(xtnsnMtchs.size())
+        app += ".app";
+    std::string bndlid = run("defaults read " + dir + "/'" + app + "'/Contents/Info CFBundleIdentifier");
+    std::string plistPath = run("launchctl dumpstate | grep -iA2 '" + bndlid + " = {' | awk '/path =/ {print $3}'");
+    return plistPath;
+}
+
+std::vector<std::string> block(std::vector<std::string> apps) {
+    std::vector<std::string> stubbornApps;
+    std::string cmd;
+    for(std::string app : apps) {
+        std::string path = getPlistPath(app, "/Applications");
+        if(path.size() == 0) {
+            path = getPlistPath(app, "/System/Library/CoreServices");
+            if(path.size() == 0) {
+                stubbornApps.push_back(app);
+                continue;
             }
         }
-    log.close();
-    for (std::string s : savedApps)
-        apps.push_back(s);
-    return apps;
-}
-
-std::vector<std::string> getSavedApps() {
-    std::vector<std::string> savedApps;
-    std::string savedApp;
-    std::ifstream log("blocklist.txt");
-    if (log.is_open())
-        while (!log.eof()) {
-            std::getline(log, savedApp);
-            if(savedApp != "" && savedApp != "\n")
-                savedApps.push_back(savedApp);
-        }
-    log.close();
-    return savedApps;
-}
-
-void block(std::vector<std::string> apps) {
-    while (1) {
-        std::string baseCmd = "taskkill /f /im ";
-        std::string cmd;
-        for (std::string s : apps) {
-            cmd = baseCmd + s;
+                
+        if (path.find("Agents") != std::string::npos) {
+            cmd = "sudo launchctl bootout gui/`id -u` " + path;
             system(cmd.c_str());
         }
+        else
+            if(path.find("Daemons") != std::string::npos) {
+                cmd = "sudo launchctl unload " + path;
+                system(cmd.c_str());
+            }
+            else
+                stubbornApps.push_back(app);
     }
+    return stubbornApps;
+}
+
+void kill(std::vector<std::string> stubbornApps) {
+    while(1)
+        for(std::string s : stubbornApps)
+            std::string cmd = "killall " + s;
 }
 
 int main(int argc, char* argv[]) {
-    system("taskkill /f /im blockStarter.exe");
-    /* system("taskkill /f /im WindowsTerminal.exe");
-    system("taskkill /f /im cmd.exe");
-    system("taskkill /f /im powershell.exe");
-    system("taskkill /f /im git-bash.exe");
-    system("taskkill /f /im bash.exe"); */
-
-    auto newApps = getArgs(argc, argv);
-    auto savedApps = getSavedApps();
-    auto apps = saveApps(newApps, savedApps);
-    block(apps);
+    std::string spawnExec(argv[0]);
+    std::string spawn = spawnExec.substr(2);
+    spawn = spawn.substr(0, 5) + '\\' + spawn.substr(5);
+    std::string baseCmd = "ps -ce | awk '/[[:space:]]\\" + spawn + "$/ {print $1}'";
+    std::string cmd = "(" + baseCmd + ") > .proc/postSpawnPIDs.txt";
+    system(cmd.c_str());
+    
+    std::vector<std::string> apps;
+    /* Skip first command-line arg, which is not name of app
+        we were asked to block, but rather of spawned proc */
+    for(int i = 1; i < argc; i++) {
+        std::string app(argv[i]);
+        apps.push_back(app);
+    }
+    
+    std::vector<std::string> stubbornApps = block(apps);
+    kill(stubbornApps);
 }
--- mac/main.cpp
+++ mac/main.cpp
@@ -1,33 +1,124 @@
 #include <iostream>
+#include <fstream>
+#include <cstdio>
 #include <string>
 #include <vector>
-#include <windows.h>
+#include <chrono>
+#include <random>
+#include <algorithm>   // std::find
 
-std::vector<std::string> getApps();
 
-std::vector<std::string> getApps() {
+std::string run(std::string, int);
+std::vector<std::string> addApps();
+std::vector<std::string> saveApps(std::vector<std::string>, std::vector<std::string>);
+std::vector<std::string> getSavedApps();
+std::vector<std::string> getListItems(std::string);
+void spawnProc(std::vector<std::string>, std::vector<std::string>);
+
+std::string run(std::string cmd, int size = 100) {
+    std::string output = "";
+    char buf[size];
+    FILE* stream = popen(cmd.c_str(), "r");
+    if (stream)
+        while (!feof(stream))
+            if (fgets(buf, size, stream) != NULL)
+                output += buf;
+    pclose(stream);
+    return output;
+}
+
+std::vector<std::string> addApps() {
     std::vector<std::string> apps;
     apps.reserve(50);
-    int i = 0;
     bool inputEmpty = false;
     while (!inputEmpty) {
-        apps.push_back("");
         inputEmpty = true;
-        std::getline(std::cin, apps[i]);
-        for (int j = 0; j < apps[i].size(); j++) {
-            char c = apps[i][j];
-            if ((int)c > 32 && c != 127 && (int)c != 255)
+        std::string app = "";
+        std::getline(std::cin, app);
+        for (char c : app) {
+            if ((int)c > 32 && (int)c != 127 && (int)c != 255)
                 inputEmpty = false;
-            else {
-                if (c == '\r' || c == '\n')
+            else
+                if (c == '\n')
                     break;
-                apps[i][j] = ':';
-            }
         }
-        i++;
+        if(!inputEmpty)
+            apps.push_back(app);
     }
     return apps;
 }
+
+std::vector<std::string> saveApps(std::vector<std::string> newApps, std::vector<std::string> savedApps) {
+    std::vector<std::string> apps;
+    apps.reserve(newApps.size() + savedApps.size());
+    system("chmod 200 .blocklist.txt");
+    std::ofstream log(".blocklist.txt", std::ios_base::app);
+    if (log.is_open())
+        for (std::string s : newApps) {
+            if (find(savedApps.begin(), savedApps.end(), s) == savedApps.end()) {
+                log << s << '\n';
+                apps.push_back(s);
+            }
+        }
+    log.close();
+    system("chmod 000 .blocklist.txt");
+    for (std::string s : savedApps)
+        apps.push_back(s);
+    return apps;
+}
+
+std::vector<std::string> getSavedApps() {
+    std::vector<std::string> savedApps;
+    std::string savedApp;
+    system("chmod 400 .blocklist.txt");
+    std::ifstream log(".blocklist.txt");
+    if (log.is_open())
+        while (!log.eof()) {
+            std::getline(log, savedApp);
+            if (savedApp != "" && savedApp != "\n")
+                savedApps.push_back(savedApp);
+        }
+    log.close();
+    system("chmod 000 .blocklist.txt");
+    return savedApps;
+}
+
+std::vector<std::string> getListItems(std::string list) {
+    std::vector<std::string> items;
+    std::string item = "";
+    for(char c : list)
+        if(c != '\n')
+            item += c;
+        else {
+            items.push_back(item);
+            item = "";
+        }
+    return items;
+}
+
+void spawnProc(std::vector<std::string> procNames, std::vector<std::string> apps) {
+    // Start by picking a random (running) process name
+    unsigned int seed = std::chrono::steady_clock::now().time_since_epoch().count();
+    std::default_random_engine re(seed);
+    std::uniform_int_distribution<unsigned long> nameDistro(0, procNames.size() - 1);
+    std::string proc = procNames[nameDistro(re)];
+    
+    // Save all PIDs associated with proc before spawn, so new PID can be identified later
+    std::string baseCmd = "ps -ce | awk '/[[:space:]]" + proc + "$/ {print $1}'";
+    std::string cmd = "mkdir .proc && (" + baseCmd + ") > .proc/preSpawnPIDs.txt";
+    std::string pidList = run(baseCmd);
+    std::vector<std::string> pids = getListItems(pidList);
+    system(cmd.c_str());
+    
+    if (find(pids.begin(), pids.end(), "") != pids.end())
+        spawnProc(procNames, apps);
+    
+    cmd = "g++ -std=c++11 block.cpp -o " + proc + " && mv " + proc + " .proc && ./.proc/" + proc;
+    for (std::string s : apps)
+        cmd += " " + s;
+    system(cmd.c_str());
+}
+
 
 int main() {
     std::cout << "\nLet's start blocking some apps. Type the name\nof the app you want to block, and then\npress "
@@ -35,12 +126,14 @@ int main() {
         << "have entered the final\napp's name, press enter/return twice to start\nblocking all the apps you've "
         << "listed.\n" << std::endl; /* \n(Warning: If you have Windows Terminal, Command Prompt,\nWindows Powershell, Git Bash, or " 
         << "any other command\nshell open, don't submit your list of apps\nuntil you're ready for that to be closed.)\n" << std::endl; */
-    auto apps = getApps();
-    HWND console = GetConsoleWindow();
-    ShowWindow(console, SW_HIDE);
-    system("taskkill /f /im appBlocker.exe");   // Kill any extant instance of appBlocker proc
-    std::string cmd = "g++ block.cpp -o appBlocker.exe && appBlocker.exe";
-    for (std::string s : apps)
-        cmd += " " + s;
-    system(cmd.c_str());
+    auto newApps = addApps();
+    auto savedApps = getSavedApps();
+    auto apps = saveApps(newApps, savedApps);
+    
+    /* HWND console = GetConsoleWindow();    // Need Mac-friendly alternative
+    ShowWindow(console, SW_HIDE); */
+    
+    std::string procList = run("ps -ce | awk '{print $4}'");
+    auto procNames = getListItems(procList);
+    spawnProc(procNames, apps);
 }
