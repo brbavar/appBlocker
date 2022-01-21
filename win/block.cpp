48ec479b1e62885067194f25747c237c280bc48f
brbavar <ben.bavar@gmail.com>
2022-01-21 15:18:24 -0500

    use shell commands to make appBlocker automatically run on startup

--- win/block.cpp
+++ win/block.cpp
@@ -1,11 +1,29 @@
 #include <fstream>
+#include <cstdio>
 #include <string>
 #include <algorithm>   // std::find 
 #include <vector>
 
+/* HEADERS BELOW PURELY FOR DEBUGGING */
+#include <iostream>
+
+
+std::string run(std::string, int);
 std::vector<std::string> getArgs(int, char* []);
 std::vector<std::string> saveApps(std::vector<std::string>, std::vector<std::string>);
 std::vector<std::string> getSavedApps();
+
+std::string run(std::string cmd, int size = 100) {
+    std::string output = "";
+    char buf[size];
+    FILE* stream = popen(cmd.c_str(), "r");
+    if (stream)
+        while (!feof(stream))
+            if (fgets(buf, size, stream) != NULL)
+                output += buf;
+    pclose(stream);
+    return output;
+}
 
 std::vector<std::string> getArgs(int argc, char* argv[]) {
     std::vector<std::string> args;
@@ -40,15 +58,36 @@ std::vector<std::string> getArgs(int argc, char* argv[]) {
 std::vector<std::string> saveApps(std::vector<std::string> newApps, std::vector<std::string> savedApps) {
     std::vector<std::string> apps;
     apps.reserve(newApps.size() + savedApps.size());
+
+    std::string uid = run("echo %username%");
+    uid = uid.substr(0, uid.size() - 1);         // Remove terminating newline character
+    std::string startupPath = "\"C:\\Users\\" + uid + "\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\";
+    startupPath += "Programs\\Startup\"";
+
+    // std::string
+
+    /* std::string wd = run("cd");
+    std::string cmd = "(for /f \"skip=7 tokens=5\" %a in('dir " + wd + "') do echo %a)";
+    std::string thisDir = run(cmd);
+    cmd = "(for /f \"skip=7 tokens=5\" %a in('dir " + startupPath + "') do echo %a)";
+    std::string startupDir = run(cmd);
+    
+    if (wd == startupPath&&)
+        std::string */
+        
     std::ofstream log("blocklist.txt", std::ios_base::app);
-    if (log.is_open())
-        for (std::string s : newApps) {
+    if (log.is_open()) {
+        std::cout << "log opened in saveApps" << '\n';
+        std::string cmd = "attrib +h " + startupPath + "\\blocklist.txt";
+        system(cmd.c_str());
+        for (std::string s : newApps)
             if (find(savedApps.begin(), savedApps.end(), s) == savedApps.end()) {
                 log << s << '\n';
                 apps.push_back(s);
             }
-        }
+    }
     log.close();
+
     for (std::string s : savedApps)
         apps.push_back(s);
     return apps;
@@ -57,14 +96,23 @@ std::vector<std::string> saveApps(std::vector<std::string> newApps, std::vector<
 std::vector<std::string> getSavedApps() {
     std::vector<std::string> savedApps;
     std::string savedApp;
+
+    /* std::string uid = run("echo %username%");
+    uid = uid.substr(0, uid.size() - 1);         // Remove terminating newline character
+    std::string logPath = "\\Users\\" + uid + "\\AppData\\Roaming\\Microsoft\\Windows\\\"Start Menu\"\\";
+    logPath += "Programs\\Startup\\blocklist.txt"; */
+
     std::ifstream log("blocklist.txt");
-    if (log.is_open())
+    if (log.is_open()) {
+        std::cout << "log opened in getSavedApps" << '\n';
         while (!log.eof()) {
             std::getline(log, savedApp);
-            if(savedApp != "" && savedApp != "\n")
+            if (savedApp != "" && savedApp != "\n")
                 savedApps.push_back(savedApp);
         }
+    }
     log.close();
+
     return savedApps;
 }
 
@@ -79,6 +127,7 @@ void block(std::vector<std::string> apps) {
     }
 }
 
+
 int main(int argc, char* argv[]) {
     system("taskkill /f /im blockStarter.exe");
     /* system("taskkill /f /im WindowsTerminal.exe");
@@ -88,7 +137,16 @@ int main(int argc, char* argv[]) {
     system("taskkill /f /im bash.exe"); */
 
     auto newApps = getArgs(argc, argv);
+    std::cout << "New apps given below:" << '\n';
+    for (std::string s : newApps)
+        std::cout << s << '\n';
     auto savedApps = getSavedApps();
+    std::cout << "Saved apps given below:" << '\n';
+    for (std::string s : savedApps)
+        std::cout << s << '\n';
     auto apps = saveApps(newApps, savedApps);
+    std::cout << "Total set of apps given below:" << '\n';
+    for (std::string s : apps)
+        std::cout << s << '\n';
     block(apps);
 }
--- win/main.cpp
+++ win/main.cpp
@@ -1,9 +1,25 @@
 #include <iostream>
+#include <fstream>
+#include <cstdio>
 #include <string>
 #include <vector>
 #include <windows.h>
 
+
+std::string run(std::string, int);
 std::vector<std::string> getApps();
+
+std::string run(std::string cmd, int size = 100) {
+    std::string output = "";
+    char buf[size];
+    FILE* stream = popen(cmd.c_str(), "r");
+    if (stream)
+        while (!feof(stream))
+            if (fgets(buf, size, stream) != NULL)
+                output += buf;
+    pclose(stream);
+    return output;
+}
 
 std::vector<std::string> getApps() {
     std::vector<std::string> apps;
@@ -16,7 +32,7 @@ std::vector<std::string> getApps() {
         std::getline(std::cin, apps[i]);
         for (int j = 0; j < apps[i].size(); j++) {
             char c = apps[i][j];
-            if ((int)c > 32 && c != 127 && (int)c != 255)
+            if ((int)c > 32 && (int)c != 127 && (int)c != 255)
                 inputEmpty = false;
             else {
                 if (c == '\n')
@@ -29,17 +45,26 @@ std::vector<std::string> getApps() {
     return apps;
 }
 
+
 int main() {
-    std::cout << "\nLet's start blocking some apps. Type the name\nof the app you want to block, and then\npress "
-        << "the enter/return key exactly once.\nRepeat this process for every app you want\nto block. Once you "
-        << "have entered the final\napp's name, press enter/return twice to start\nblocking all the apps you've "
-        << "listed.\n" << std::endl; /* \n(Warning: If you have Windows Terminal, Command Prompt,\nWindows Powershell, Git Bash, or " 
-        << "any other command\nshell open, don't submit your list of apps\nuntil you're ready for that to be closed.)\n" << std::endl; */
+    std::cout << '\n' << "Let's start blocking some apps. Type the name" << '\n' << "of the app you want to block, and then" << '\n' << "press " <<
+        "the enter/return key exactly once." << '\n' << "Repeat this process for every app you want" << '\n' << "to block. Once you " <<
+        "have entered the final" << '\n' << "app's name, press enter/return twice to start" << '\n' << "blocking all the apps you've " <<
+        "listed." << '\n' << '\n'; /* (Warning: If you have Windows Terminal, Command Prompt,\nWindows Powershell, Git Bash, or "
+        << "any other command\nshell open, don't submit your list of apps\nuntil you're ready for that to be closed.)" << '\n' << '\n'; */
     auto apps = getApps();
+
     HWND console = GetConsoleWindow();
     ShowWindow(console, SW_HIDE);
-    system("taskkill /f /im appBlocker.exe");   // Kill any extant instance of appBlocker proc
-    std::string cmd = "g++ block.cpp -o appBlocker.exe && appBlocker.exe";
+    
+    system("taskkill /f /im appBlocker.exe");    // Kill any extant instance of appBlocker proc
+    
+    std::string uid = run("echo %username%");
+    uid = uid.substr(0, uid.size() - 1);         // Remove terminating newline character
+    std::string startupPath = "\"\\Users\\" + uid + "\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu";
+    startupPath += "\\Programs\\Startup";
+    std::string cmd = "move block.cpp " + startupPath + "\" && g++ -std=c++11 " + startupPath + "\\block.cpp\" -o " + startupPath + "\\appBlocker.exe\"";
+    cmd += " && move " + startupPath + "\\block.cpp\" . && " + startupPath + "\\appBlocker.exe\"";
     for (std::string s : apps)
         cmd += " " + s;
     system(cmd.c_str());
